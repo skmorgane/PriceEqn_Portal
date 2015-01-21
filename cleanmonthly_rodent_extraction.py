3c6a47e63863497b23d23f939099d3323439576a
Morgan Ernest <morgan@weecology.org>
2015-01-21 09:12:32 -0700

    Removed line that was commented out and no longer needed. This commit marks the beginning of the rewrite of this code to incorporate real zeros into the average treatment values

--- cleanmonthly_rodent_extraction.py
+++ cleanmonthly_rodent_extraction.py
@@ -14,7 +14,7 @@ def check_for_missing_periods(data):
         outcome = "No Missing Periods"
     return outcome
 
-#extracting just the raw data needed for the project
+#extracting just the raw data needed for the project from Portal Database on Server
 query_rats = """SELECT Rodents.mo, Rodents.dy, Rodents.yr, Rodents.period, Rodents.plot, Plots.`Type Code`, Rodents.note1, Rodents.species, Rodents.wgt 
            FROM Rodents JOIN Plots JOIN SPECIES
            ON Rodents.plot=Plots.`Plot Number`
@@ -29,8 +29,6 @@ engine = sqlalchemy.create_engine('mysql+pymysql://morgan:{}@{}:{}/{}'.format(cr
 raw_data=pd.read_sql_query(query_rats, engine)
 plot_info = pd.read_sql_table("Plots", engine)
 
-
-
 #inserts species average mass for missing values
 raw_data['wgt'] = raw_data[['species','wgt']].groupby("species").transform(lambda x: x.fillna(x.mean()))
 raw_data['Type'] = np.where(raw_data['Type Code'] == 'CO', 1, 0)
@@ -41,6 +39,7 @@ print check_for_missing_periods(raw_data['period'])
 #uses individual mass to calculate an individual's energy use
 raw_data['energy'] = 5.69 * raw_data['wgt'] ** 0.75
 
+#calculates mean energy use per species per plot for each trapping session
 plot_sums = raw_data[['period', 'plot', 'Type', 'species', 'energy']].groupby(['period', 'plot', 'Type', 'species']).sum()
 plot_sums.reset_index(inplace=True)
-treatment_sums = plot_sums[['period', 'Type', 'species', 'energy']].groupby(['period', 'Type', 'species']).mean()
+treatment_avg = plot_sums[['period', 'Type', 'species', 'energy']].groupby(['period', 'Type', 'species']).mean()

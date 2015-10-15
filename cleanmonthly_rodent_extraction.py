a2c05fe80b1e22e0f0b09857844a8bc1e0fd131a
Morgan Ernest <morgan@weecology.org>
2015-10-15 15:48:25 -0400

    Updated documentation for functions and main code

--- cleanmonthly_rodent_extraction.py
+++ cleanmonthly_rodent_extraction.py
@@ -6,6 +6,22 @@ import json
 ###### FUNCTIONS
 
 def check_for_missing_periods(data):
+    
+    """ Checks for missing periods
+    
+    Checks that the number of periods calculated by length of unique period
+    is the same as the length calculated by compariong first and last period
+    number. A mismatch between these approaches indicates that there is a
+    trapping period missing from the datafile. If the code continues, 
+    issues will ensue
+    
+    Args:
+        data: column from imported data table which should consist of integer
+            values.
+    
+    Returns:
+        A message about the state of the test
+    """
     Periods_unique=data.unique()
     Total_periods =data.max() - (data.min() - 1)
     
@@ -18,6 +34,11 @@ def check_for_missing_periods(data):
 ######  MAIN CODE
 
 # Extracts subset of database from server that is needed for the project
+# This requires access to the json file to log on to the server.
+
+# Query: Pulls out each rodent record that matches the following filter: Not on
+#        A removal plot, captured after period 118 but before 429 (to match
+#        Thibault et al 2008), is an identified rodent species.
 
 query_rats = """SELECT Rodents.mo, Rodents.dy, Rodents.yr, 
                 Rodents.period, Rodents.plot, Plots.`Type Code`, 
@@ -30,21 +51,25 @@ query_rats = """SELECT Rodents.mo, Rodents.dy, Rodents.yr,
                 AND (SPECIES.Rodent = 1) AND (SPECIES.Unknown = 0)
                 """
 credentials = json.load(open("db_credentials.json", "r"))
-engine = sqlalchemy.create_engine('mysql+pymysql://morgan:{}@{}:{}/{}'.format(credentials['password'], credentials['host'], credentials['port'], credentials['database']))
+engine = sqlalchemy.create_engine('mysql+pymysql://morgan:{}@{}:{}/{}'
+                                  .format(credentials['password'], 
+                                          credentials['host'], 
+                                          credentials['port'], 
+                                          credentials['database']))
 raw_data=pd.read_sql_query(query_rats, engine)
 plot_info = pd.read_sql_table("Plots", engine)
 plot_info.rename(columns={'Plot Number': 'plot'}, inplace=True)
 print check_for_missing_periods(raw_data['period'])
 
-# inserts species average mass for missing values & calculates 
-# individual energy use
+# inserts species average mass for missing values, calculates 
+# individual energy use, and recodes treatment codes to a numeric
 
 raw_data['wgt'] = raw_data[['species','wgt']].groupby("species").transform(lambda x: x.fillna(x.mean()))
 raw_data['Type'] = raw_data['Type Code'].map({'CO': 0, 'LK': 1, 
                                               'RE':2, 'SK':1})
 raw_data['energy'] = 5.69 * raw_data['wgt'] ** 0.75
 
-# imports trapping table, adds plot type info to table, calculates 
+# imports trapping table, adds treatment type info to trapping table, calculates 
 # number of plots censused per period
 
 Trapping_Table = pd.read_csv('Trapping_Table.csv')
@@ -58,7 +83,10 @@ period_plot_count = Trapping_Table[['period', 'Type', 'sampled']].groupby(['peri
 period_plot_count.reset_index(inplace=True)
 
 # calculates mean energy use per species per treatment for each trapping 
-# session
+# session using actual number of plots sampled as determined from the trapping
+# table.  Because sometimes plots have no rodents you cannot just average by
+# the number of unique plots in the database
+
 treatment_sums = raw_data[['period', 'plot', 'Type', 'species', 'energy'
                            ]].groupby(['period', 'Type', 'species']).sum()
 treatment_sums.reset_index(inplace=True)
@@ -77,7 +105,7 @@ treatment_sums = pd.merge(treatment_sums, JulianDate_for_period, how='left',
                           on=['period'])
 
 # formatting and output for analysis
+
 treatment_data_export = treatment_sums.drop(['plot', 'energy', 'sampled'], 
-                                            axis = 1)
-                                              
+                                            axis = 1)                                             
 treatment_data_export.to_csv("Portal_Rodents_PriceProject.csv")
